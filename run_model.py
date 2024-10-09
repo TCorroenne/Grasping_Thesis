9437af513ef014fb300cc040e47656c12e469595
TCorroenne <timothee.corro@gmail.com>
2024-10-09 14:26:50 +0200

    load images and network in run model

--- run_model.py
+++ run_model.py
@@ -1,6 +1,11 @@
 import argparse
 import torch
-# import file from PEGG-Net folder:
+import cv2
+import numpy as np
+# import files from PEGG-Net folder:
+import PEGG_Net.models.peggnet as peggnet
+# import custom files:
+import utils.image_processing as ip
 
 
 def parse_args():
@@ -19,15 +24,33 @@ def parse_args():
 def main():
     args = parse_args()
     print(args)
+    assert args.use_rgb or args.use_depth, 'At least one of RGB or depth must be used'
+    
+    # Determine the number of input channels based on the input type
+    input_channels = 4
+    if args.use_rgb and not args.use_depth:
+        input_channels = 3
+    elif args.use_depth and not args.use_rgb:
+        input_channels = 1
+    # Load the images
+    rgb = cv2.imread(args.rgb, cv2.IMREAD_UNCHANGED)
+    depth = cv2.imread(args.depth, cv2.IMREAD_UNCHANGED)
+
+    # Load the model
     assert args.model is not None or args.state_dict is not None, 'Either model or state-dict must be specified'
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device: {}'.format(device))
     net = None
     if args.model is not None:
         print('Loading model from {}'.format(args.model))
         net = torch.load(args.model, map_location=device)
     else:
         print('Loading model from state dict {}'.format(args.state_dict))
-        net 
+        net = peggnet.PEGGNet(input_channels=input_channels)
+        net.load_state_dict(torch.load(args.state_dict, map_location=device))
+    assert net is not None, 'Model not loaded'
+    print('Model loaded')
+    
     
 
 if __name__ == '__main__':
