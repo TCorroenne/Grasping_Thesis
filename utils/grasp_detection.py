7bf33476cd0968d51973d0d7205b6c538a30d88a
TCorroenne <timothee.corro@gmail.com>
2024-10-10 14:28:44 +0200

    added grasp detection

---
+++ utils/grasp_detection.py
@@ -0,0 +1,31 @@
+from skimage.feature import peak_local_max
+from PEGG_Net.utils.dataset_processing.grasp import Grasp
+
+def detect_grasps(q_img, ang_img, width_img=None, no_grasps=1, mask=None):
+    """
+    Detect grasps in a GG-CNN output.
+    :param q_img: Q image network output
+    :param ang_img: Angle image network output
+    :param width_img: (optional) Width image network output
+    :param no_grasps: Max number of grasps to return
+    :return: list of Grasps
+    """
+    local_max = peak_local_max(q_img, min_distance=20, threshold_abs=0.2, num_peaks=no_grasps)
+
+    grasps = []
+    for grasp_point_array in local_max:
+        grasp_point = tuple(grasp_point_array)
+        if mask is not None and not mask[grasp_point]:
+            # Invalid grasp point, not in mask
+            continue
+
+        grasp_angle = ang_img[grasp_point]
+
+        g = Grasp(grasp_point, grasp_angle)
+        if width_img is not None:
+            g.length = width_img[grasp_point]
+            g.width = g.length/2
+
+        grasps.append(g)
+
+    return grasps
