96c1eb046028be12ef1676774b49cfef1ad86e20
TCorroenne <timothee.corro@gmail.com>
2024-11-21 11:40:04 +0100

    implemented custom dataset in dataset selector

--- utility/data/__init__.py
+++ utility/data/__init__.py
@@ -2,6 +2,9 @@ def get_custom_dataset(dataset_name):
     if dataset_name == 'cornell':
         from .cornell_data import CornellDataset
         return CornellDataset
+    elif dataset_name == 'custom':
+        from .cornell_data import CustomDataset
+        return CustomDataset
     # elif dataset_name == 'jacquard':
     #     from .jacquard_data import JacquardDataset
     #     return JacquardDataset
